51c07f5020319cddfd1415917c1a981119ef9615
Brandon Fan <bxfan@umich.edu>
2021-02-16 11:46:49 -0500

    add dragability

--- src/app/prayer-wall/components/prayer-items-container/prayer-items-container.component.ts
+++ src/app/prayer-wall/components/prayer-items-container/prayer-items-container.component.ts
@@ -1,9 +1,11 @@
+import { UtilitiesService } from './../../../shared/services/utilities/utilities.service';
 import { PrayerItemsService } from './../../../shared/services/prayer-items/prayer-items.service';
 import { Component, Input, OnInit, AfterViewInit } from '@angular/core';
 import { PrayerItem } from 'src/app/shared/interfaces/prayer-item';
-import { tap } from 'rxjs/operators';
+import { delay, map, tap } from 'rxjs/operators';
 
 declare const $: any;
+declare const Draggabilly: any;
 @Component({
   selector: 'app-prayer-items-container',
   templateUrl: './prayer-items-container.component.html',
@@ -13,15 +15,45 @@ export class PrayerItemsContainerComponent implements OnInit, AfterViewInit {
   @Input() churchId: string;
   items: PrayerItem[] = [];
   loading = false;
-  constructor(private itemService: PrayerItemsService) { }
+  count = 0;
+  grid: any;
+  constructor(private itemService: PrayerItemsService, private utilities: UtilitiesService) { }
 
   ngOnInit(): void {
-    this.itemService.getItemsById(this.churchId).subscribe((res) => {
+    this.itemService.getItemsById(this.churchId)
+      .pipe(
+        map((res) => {
+          return this.utilities.shuffle(res);
+        })
+      )
+      .subscribe(async (res) => {
       this.items = res;
+      if (this.count > 0) {
+        await this.delay(500);
+        $('.items-row').packery('destroy');
+        this.grid = $('.items-row').packery();
+        this.grid.find('.item').each((i, gridItem) => {
+          this.grid.packery('bindDraggabillyEvents', new Draggabilly(gridItem));
+        });
+      }
+      this.count++;
     });
   }
 
-  ngAfterViewInit() {
+  async ngAfterViewInit() {
+    await this.delay(1500);
+    this.grid = $('.items-row').packery();
+    this.grid.find('.item').each((i, gridItem) => {
+      this.grid.packery('bindDraggabillyEvents', new Draggabilly(gridItem));
+    });
+  }
+
+  delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  checkPosition() {
+    console.log('checking position...');
   }
 
 }
